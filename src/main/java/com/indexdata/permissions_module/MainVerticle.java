98ce3067a56e39ab8ec8d0cfc84d0965bd13bbe9
Kurt Nordstrom <kurt.e.nordstrom@gmail.com>
2016-08-15 16:31:53 -0400

    Completed implementation of store

--- src/main/java/com/indexdata/permissions_module/MainVerticle.java
+++ src/main/java/com/indexdata/permissions_module/MainVerticle.java
@@ -5,9 +5,15 @@
  */
 package com.indexdata.permissions_module;
 
+import com.indexdata.permissions_module.impl.MongoPermissionsStore;
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+import io.vertx.core.http.HttpMethod;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.mongo.MongoClient;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
+import io.vertx.ext.web.handler.BodyHandler;
 
 /**
  *
@@ -16,11 +22,66 @@ import io.vertx.ext.mongo.MongoClient;
 public class MainVerticle extends AbstractVerticle {
   private MongoClient mongoClient;
   private String authApiKey;
+  private PermissionsStore store;
   
-  public void start() {
+  public void start(Future<Void> future) {
     authApiKey = System.getProperty("auth.api.key", "VERY_WEAK_KEY");
     
     String mongoURL = System.getProperty("mongo.url", "mongodb://localhost:27017");
     mongoClient = MongoClient.createShared(vertx, new JsonObject().put("connection_string", mongoURL));
+    store = new MongoPermissionsStore(mongoClient);
+    
+    Router router = Router.router(vertx);
+    router.post("/users").handler(BodyHandler.create());
+    router.post("/permissions").handler(BodyHandler.create());
+    router.get("/users/:username").handler(this::handleUser);
+    router.get("/users/:username/permissions").handler(this::handleUserPermission);
+    router.post("/users/:username/permissions").handler(this::handleUserPermission);
+    router.delete("/users/:username/permissions/:permission_name").handler(this::handleUserPermission);
+    router.post("/users").handler(this::handleUser);
+    router.delete("/users/:username").handler(this::handleUser);
+    router.get("/permissions/:permission_name").handler(this::handlePermission);
+    router.post("/permissions").handler(this::handlePermission);
+    
+  }
+  
+  private void handleUser(RoutingContext context) {
+    if(context.request().method() == HttpMethod.POST) {
+      String username;
+      try {
+        username = context.getBodyAsString();
+      } catch(Exception e) {
+        context.response()
+                .setStatusCode(400)
+                .end("Please provide a username");
+        return;
+      }
+      store.addUser(username).setHandler(res -> {
+        if(res.succeeded()) {
+          context.response()
+                  .setStatusCode(201)
+                  .end("User added");
+        } else {
+          context.response()
+                  .setStatusCode(500)
+                  .end("Unable to add user");
+        }
+      });
+    } else if(context.request().method() == HttpMethod.DELETE) {
+      
+    }
+    else {
+      context.response()
+              .setStatusCode(400)
+              .end("Unsupported method");
+    }
+  }
+  
+  private void handlePermission(RoutingContext context) {
+    
+  }
+  
+  private void handleUserPermission(RoutingContext context) {
+  
   }
 }
--- src/main/java/com/indexdata/permissions_module/impl/MongoPermissionsStore.java
+++ src/main/java/com/indexdata/permissions_module/impl/MongoPermissionsStore.java
@@ -11,6 +11,7 @@ import io.vertx.core.Future;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.mongo.MongoClient;
+import io.vertx.ext.mongo.UpdateOptions;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
@@ -22,6 +23,7 @@ import java.util.List;
  */
 public class MongoPermissionsStore implements PermissionsStore {
   private MongoClient mongoClient;
+  private UpdateOptions updateOptions = new UpdateOptions().setMulti(true);
   
   public MongoPermissionsStore(MongoClient mongoClient) {
     this.mongoClient = mongoClient;
@@ -75,6 +77,72 @@ public class MongoPermissionsStore implements PermissionsStore {
     });
     return future;
   }
+  
+  @Override
+  public Future<JsonArray> getSubPermissions(String permission) {
+    JsonObject query = new JsonObject().put("permission_name", permission);
+    JsonArray permList = new JsonArray();
+    Future<JsonArray> future = Future.future();
+    mongoClient.find("permissions", query, res -> {
+      if(!res.succeeded()) {
+        future.fail("Query unsuccessful");
+      } else {
+        JsonObject permObject = res.result().get(0);
+        future.complete(permObject.getJsonArray("sub_permissions"));
+      }
+    });
+    return future;        
+  }
+
+  @Override
+  public Future<JsonArray> getExpandedPermissions(String permission) {
+    JsonObject query = new JsonObject().put("permission_name", permission);
+    JsonArray permList = new JsonArray();
+    Future<JsonArray> future = Future.future();
+    mongoClient.find("permissions", query, res -> {
+      if(res.succeeded() && res.result().size() > 0) {
+        /*
+        If there are no subpermissions, go ahead and complete the future with the
+        given value of the JsonArray
+        
+        If there are subpermissions, create a list of new futures, by calling
+        walkPerms for each sub permission, then create a composite future from
+        these new futures, with a handler that completes the original
+        future when they return
+        */
+        JsonObject permObj = res.result().get(0);
+        permList.add(permission);
+        JsonArray subPerms = permObj.getJsonArray("sub_permissions");
+        if(!subPerms.isEmpty()) {
+          LinkedList<Future> futureList = new LinkedList<>();
+          for(Object o : subPerms) {
+            String sub = (String)o;
+            Future<JsonArray> newFuture = getExpandedPermissions(sub);
+            futureList.add(newFuture);
+          }
+          CompositeFuture compositeFuture = CompositeFuture.all(futureList);
+          compositeFuture.setHandler(res2 -> {
+            if(res2.succeeded()) {
+              //Get output of contained futures and complete the future here
+              for(Future f : futureList) {
+                JsonArray arr = (JsonArray)f.result();
+                for(Object o : arr) {
+                  permList.add(o);
+                }
+              }
+              future.complete(permList);
+            } else {
+              future.fail("Unable to populate permissions");
+            }
+          });
+        } else {
+          future.complete(permList);
+        }
+      }
+    });
+    return future;
+  }
+
 
   @Override
   public Future<Boolean> removePermission(String permission) {
@@ -89,10 +157,10 @@ public class MongoPermissionsStore implements PermissionsStore {
         JsonObject subQuery = new JsonObject().put("sub_permissions", new JsonObject()
           .put("$in", new JsonArray().add(permission)));
          */
-        JsonObject subUpdate = new JsonObject().put("$pull", new JsonObject()
-          .put("sub_permissions", new JsonObject()
-            .put("$in", new JsonArray().add(permission))));
-        mongoClient.update("permissions", new JsonObject(), subUpdate, res2-> {
+        JsonObject subUpdate = new JsonObject()
+                .put("$pullAll", new JsonObject()
+                  .put("sub_permissions", new JsonArray().add(permission)));
+        mongoClient.updateWithOptions("permissions", new JsonObject(), subUpdate, updateOptions, res2-> {
           if(!res2.succeeded()) {
             future.fail("Unable to remove sub permissions");
           } else {
@@ -130,98 +198,106 @@ public class MongoPermissionsStore implements PermissionsStore {
 
   @Override
   public Future<Boolean> addUser(String user) {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    JsonObject query = new JsonObject().put("user_name", user);
+    Future<Boolean> future = Future.future();
+    mongoClient.find("users", query, res-> {
+      if(res.result().size() > 0) {
+        future.complete(false);
+      } else {
+        JsonObject insert = new JsonObject()
+                .put("user_name", user)
+                .put("user_permissions", new JsonArray());
+        mongoClient.insert("users", insert, res2 -> {
+          if(res2.succeeded()) {
+            future.complete(true);
+          } else {
+            future.fail("Unable to insert new user");
+          }
+        });
+      }
+    });
+    return future;
   }
 
   @Override
   public Future<Boolean> removeUser(String user) {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    JsonObject query = new JsonObject().put("user_name", user);
+    Future<Boolean> future = Future.future();
+    mongoClient.find("users", query, res-> {
+      if(res.result().size() > 0) {
+        future.complete(false);
+      } else {
+        mongoClient.remove("users", query, res2-> {
+          if(!res.succeeded()) {
+            future.fail("Unable to remove user");
+          } else {
+            future.complete(true);
+          }
+        });
+      }
+    });
+    return future;
   }
 
   @Override
   public Future<Boolean> addPermissionToUser(String user, String permission) {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    JsonObject query = new JsonObject().put("user_name", user);
+    Future<Boolean> future = Future.future();
+    mongoClient.find("users", query, res-> {
+      if(res.result().size() > 0) {
+        future.complete(false);
+      } else {
+        JsonObject push = new JsonObject()
+                .put("$push", new JsonObject()
+                  .put("user_permissions", permission));
+        mongoClient.update("users", query, push, res2 -> {
+          if(!res2.succeeded()) {
+            future.fail("Unable to add permission");
+          } else {
+            future.complete(true);
+          }
+        });
+      }
+    });
+    return future;
   }
 
   @Override
   public Future<Boolean> removePermissionFromUser(String user, String permission) {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    JsonObject query = new JsonObject().put("user_name", user);
+    Future<Boolean> future = Future.future();
+    mongoClient.find("users", query, res-> {
+      if(res.result().size() > 0) {
+        future.complete(false);
+      } else {
+        JsonObject update = new JsonObject().put("$pullAll", new JsonObject()
+          .put("user_permissions", new JsonArray().add(permission)));
+        mongoClient.update("users", query, update, res2 -> {
+          if(!res2.succeeded()) {
+            future.fail("Unable to remove permission");
+          } else {
+            future.complete(true);
+          }
+        });
+      }       
+    });
+    return future;
   }
 
   @Override
   public Future<JsonArray> getPermissionsForUser(String user) {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-  }
-
-  @Override
-  public Future<JsonArray> getSubPermissions(String permission) {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-  }
-
-  @Override
-  public Future<JsonArray> getExpandedPermissions(String permission) {
+    JsonObject query = new JsonObject().put("user_name", user);
     Future<JsonArray> future = Future.future();
-    Future<Void> futureChain = Future.future();
-        
-    
-    LinkedList<String> permissionNames = new LinkedList<>();
-    permissionNames.add(permission);
-    JsonObject query = new JsonObject().put("permission_name", permission);
-    mongoClient.find("permissions", query, res -> {
-      if(!res.succeeded()) {
-        future.fail("Unable to complete query");
+    mongoClient.find("users", query, res-> {
+      if(res.result().size() < 1) {
+        future.fail("No such user");
       } else {
-        LinkedList<JsonObject> permissionObjectList = new LinkedList<>();
-        JsonObject permissionObject = res.result().get(0);
-        permissionObjectList.add(permissionObject);
-        while(!permissionObjectList.isEmpty()) {
-          JsonObject curPermObj = permissionObjectList.pop();
-          JsonArray subPermissions = curPermObj.getJsonArray("sub_permissions");
-          for(Object o : subPermissions) {
-            String sub = (String)o;
-          }
-          
-        }
-      }
-    });
-    return future;
-  }
-  
-  private Future<JsonArray> walkPerms(String permission) {
-    JsonObject query = new JsonObject().put("permission_name", permission);
-    JsonArray permList = new JsonArray();
-    Future<JsonArray> future = Future.future();
-    mongoClient.find("permissions", query, res -> {
-      if(res.succeeded() && res.result().size() > 0) {
-        /*
-        If there are no subpermissions, go ahead and complete the future with the
-        given value of the JsonArray
-        
-        If there are subpermissions, create a list of new futures, by calling
-        walkPerms for each sub permission, then create a composite future from
-        these new futures, with a handler that completes the original
-        future when they return
-        */
-        JsonObject permObj = res.result().get(0);
-        permList.add(permission);
-        JsonArray subPerms = permObj.getJsonArray("sub_permissions");
-        if(!subPerms.isEmpty()) {
-          LinkedList<Future> futureList = new LinkedList<>();
-          for(Object o : subPerms) {
-            String sub = (String)o;
-            Future<JsonArray> newFuture = walkPerms(sub);
-            futureList.add(newFuture);
-          }
-          CompositeFuture compositeFuture = CompositeFuture.all(futureList);
-          compositeFuture.compose(res2 -> {
-            //Get output of contained futures and complete the future here
-          }, future);
-        } else {
-          future.complete(new JsonArray().add(permission));
-        }
+        JsonObject userObject = res.result().get(0);
+        future.complete(userObject.getJsonArray("user_permissions"));
       }
     });
     return future;
   }
 
+  
 }
