3cd7c67c66f4414d4991f747d097866e2dc93989
Kurt Nordstrom <kurt.e.nordstrom@gmail.com>
2017-01-09 14:01:44 -0500

    Working on implementation

--- src/main/java/org/folio/rest/impl/PermsAPI.java
+++ src/main/java/org/folio/rest/impl/PermsAPI.java
@@ -7,21 +7,38 @@ package org.folio.rest.impl;
 
 import io.vertx.core.AsyncResult;
 import io.vertx.core.Context;
+import io.vertx.core.Future;
 import io.vertx.core.Handler;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
 import java.util.Map;
 import javax.ws.rs.core.Response;
 import org.folio.rest.jaxrs.model.Permission;
 import org.folio.rest.jaxrs.model.PermissionPatch;
 import org.folio.rest.jaxrs.model.PermissionUser;
 import org.folio.rest.jaxrs.resource.PermsResource;
+import org.folio.rest.persist.Criteria.Limit;
+import org.folio.rest.persist.Criteria.Offset;
+import org.folio.rest.persist.cql.CQLWrapper;
 import org.folio.rest.tools.messages.MessageConsts;
 import org.folio.rest.tools.messages.Messages;
+import org.folio.rest.tools.utils.TenantTool;
+import org.z3950.zing.cql.cql2pgjson.CQL2PgJSON;
 
 /**
  *
  * @author kurt
  */
 public class PermsAPI implements PermsResource {
+  
+  private static final String TABLE_NAME_PERMS = "permissions";
+  private static final String OKAPI_TENANT_HEADER = "X-Okapi-Tenant";
+  private final Logger logger = LoggerFactory.getLogger(PermsAPI.class);
+  
+  private CQLWrapper getCQL(String query, int limit, int offset){
+    CQL2PgJSON cql2pgJson = new CQL2PgJSON(TABLE_NAME_PERMS + ".jsonb");
+    return new CQLWrapper(cql2pgJson, query).setLimit(new Limit(limit)).setOffset(new Offset(offset));
+  }
   
   private final Messages messages = Messages.getInstance();
 
@@ -31,7 +48,23 @@ public class PermsAPI implements PermsResource {
           Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext)
           throws Exception {
     try {
-      
+      vertxContext.runOnContext(v -> {
+        CQLWrapper cql = getCQL(query, length, start);
+        String tenantId = TenantTool.calculateTenantId(okapiHeaders.get(OKAPI_TENANT_HEADER));
+        String[] fieldList = {"*"};
+        try {
+          
+        } catch(Exception e) {
+          if(e.getCause() != null && e.getCause().getClass().getSimpleName().contains("CQLParseException")) {
+                logger.debug("BAD CQL");
+                asyncResultHandler.handle(Future.succeededFuture(GetPermsUsersResponse.withBadRequest() withPlainBadRequest(
+                        "CQL Parsing Error for '" + query + "': " + e.getLocalizedMessage())));
+              } else {
+                asyncResultHandler.handle(io.vertx.core.Future.succeededFuture(
+                              GetPermsUsersResponse.withInternalServerError()));
+              }
+        }
+      });      
     } catch(Exception e) {
       asyncResultHandler.handle(io.vertx.core.Future.succeededFuture(
                         GetPermsUsersResponse.withInternalServerError()));
