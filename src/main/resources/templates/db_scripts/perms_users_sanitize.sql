7cbda45fc416d823b65928f96b27d28dec646359
Julian Ladisch <julianladisch@users.noreply.github.com>
2022-05-06 16:21:34 +0200

    MODPERMS-195: perms_users_sanitize (userId is null) ignores tenant
    ```
    IF NOT EXISTS (SELECT 1 FROM pg_constraint WHERE conname = 'permissions_users_userid_not_null') THEN
    ```
    
    in perms_users_sanitize.sql checks if the constraint exists for any tenant.
    This doesn't execute the code for all but the first tenant in a multi-tenant environment.
    
    (cherry picked from commit 704e9d605f67eb01e4866db3ee82e25d218a51c6)

--- src/main/resources/templates/db_scripts/perms_users_sanitize.sql
+++ src/main/resources/templates/db_scripts/perms_users_sanitize.sql
@@ -1,10 +1,15 @@
 DO $$
 BEGIN
-  IF NOT EXISTS (SELECT 1 FROM pg_constraint WHERE conname = 'permissions_users_userid_not_null') THEN
-    DELETE FROM ${myuniversity}_${mymodule}.permissions_users WHERE jsonb->>'userId' IS NULL;
+  BEGIN
     ALTER TABLE ${myuniversity}_${mymodule}.permissions_users
-      ADD CONSTRAINT permissions_users_userid_not_null CHECK (jsonb->>'userId' IS NOT NULL);
-  END IF;
+        ADD CONSTRAINT permissions_users_userid_not_null CHECK (jsonb->>'userId' IS NOT NULL);
+  EXCEPTION
+    WHEN duplicate_object THEN
+      -- nothing to do
+    WHEN check_violation THEN
+      DELETE FROM ${myuniversity}_${mymodule}.permissions_users WHERE jsonb->>'userId' IS NULL;
+      ALTER TABLE ${myuniversity}_${mymodule}.permissions_users
+          ADD CONSTRAINT permissions_users_userid_not_null CHECK (jsonb->>'userId' IS NOT NULL);
+  END;
 END;
 $$;
-
